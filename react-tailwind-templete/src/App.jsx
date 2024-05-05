1f01fdb4ef7cc968662be7e5fff1476e2af37d32
Suheer Zahid <150055579+CodeWithSuheer@users.noreply.github.com>
2024-05-05 05:20:30 +0500

    first commit

---
+++ react-tailwind-templete/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='mt-20 mb-3 text-4xl font-semibold text-center'>React + Tailwind CSS Template</h2>
+      <p className='text-2xl text-center'>My By Suheer Zahid</p>
+    </>
+  )
+}
+
+export default App
